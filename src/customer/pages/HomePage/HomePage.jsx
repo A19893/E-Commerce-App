d4b8f70f189a5c6653d753f8f60f1131a550781c
A19893 <yasharora2678@gmail.com>
2024-03-07 17:31:27 +0530

    first commit

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import MainCarosel from '../../components/HomeCarosel/MainCarosel'
+import HomeSectionCarosel from '../../components/HomeSectionCarosel/HomeSectionCarosel'
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarosel />
+      <div className="space-y-10 py-20 flex flex-col justify-center">
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage
